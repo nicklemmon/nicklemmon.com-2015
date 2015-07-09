76948e68cc618bf3e09491475c6f81b8812accc1
Nick Lemmon <nick.lemmon@gmail.com>
2015-07-09 11:43:51 -0400

    Small detail changes

--- js/script.js
+++ js/script.js
@@ -6,35 +6,27 @@ $( document ).ready(function() {
         $(".work-container").addClass("remove"); /* ...add the class .remove to .work-container... */
         $(".work-focus").addClass("slide"); /* ...and finally ad the class .slide to .work-focus */
     });
-    
     $(".return").click(function() { /* when the user clicks on .return... */
         $(".work-focus").removeClass("slide"); /* ...remove the class .slide from .work-focus... */
         $(".work-focus").addClass("remove"); /* ...add the class .remove to .work-focus... */
         $(".work-container").delay(100).queue(function() {
                                                 $(this).removeClass("remove");
                                                 $(this).dequeue();
-                                               })
-             /* ...and remove the class .remove from .work-container */
+                                               });  /* ...and remove the class .remove from .work-container */
     });
     
     /*===== functions for section.resume to 'open' each resume skill and display the <dd> =====*/
     $(".definition").click(function(){
-        
         var dd = $(this).children("dd");
-        
         if (  !(dd.hasClass("show"))  ) { /* if the <dd> element does NOT have a class of .show... */
             dd.addClass("show"); /* ...then add a class of .show... */
             $(this).children("dt").addClass("pulse"); /* ...and add a class of .pulse to the <dt> element */
         }
-        
         else { /* if the <dd> element DOES have a class of .show... */
             dd.removeClass("show"); /* ...then remove a class of .show... */
             $(this).children("dt").removeClass("pulse"); /* ...and remove a class of .pulse */
         }
-        
     });
-    
-    
     
     /*===== smooth scrolling function from https://css-tricks.com/snippets/jquery/smooth-scrolling/ =====*/
     $('a[href*=#]:not([href=#])').click(function() { 
@@ -49,5 +41,6 @@ $( document ).ready(function() {
           }
         }
     });
+    
     
 });
