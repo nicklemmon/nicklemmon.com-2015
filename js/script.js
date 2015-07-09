d308e7bd94a163c712f33e5d03b175cf67d3d950
Nick Lemmon <nick.lemmon@gmail.com>
2015-07-08 22:20:54 -0400

    Making .portfolio smoother, changing .resume animations

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 $( document ).ready(function() {
 
     /*===== functions for section.portfolio to bring in detailed view of each .work-item =====*/
-    $(".work-item").click(function() {
-        $(".work-focus").removeClass("remove");
-        $(".work-container").addClass("remove");
-        $(".work-focus").addClass("slide");
+    $(".work-item").click(function() { /* when the user clicks on a .work-item... */
+        $(".work-focus").removeClass("remove"); /* ...remove the class .remove from .work-focus... */
+        $(".work-container").addClass("remove"); /* ...add the class .remove to .work-container... */
+        $(".work-focus").addClass("slide"); /* ...and finally ad the class .slide to .work-focus */
     });
     
-    $(".return").click(function() {
-        $(".work-focus").removeClass("slide");
-        $(".work-focus").addClass("remove");
-        $(".work-container").removeClass("remove");
+    $(".return").click(function() { /* when the user clicks on .return... */
+        $(".work-focus").removeClass("slide"); /* ...remove the class .slide from .work-focus... */
+        $(".work-focus").addClass("remove"); /* ...add the class .remove to .work-focus... */
+        $(".work-container").delay(100).queue(function() {
+                                                $(this).removeClass("remove");
+                                                $(this).dequeue();
+                                               })
+             /* ...and remove the class .remove from .work-container */
     });
     
     /*===== functions for section.resume to 'open' each resume skill and display the <dd> =====*/
@@ -20,14 +24,12 @@ $( document ).ready(function() {
         
         if (  !(dd.hasClass("show"))  ) { /* if the <dd> element does NOT have a class of .show... */
             dd.addClass("show"); /* ...then add a class of .show... */
-            $(this).children("dt").removeClass("right-spin"); /* ...and remove a class of .right-spin if it's there... */
-            $(this).children("dt").addClass("left-spin"); /* ...and add a class of .left-spin to the <dt> element */
+            $(this).children("dt").addClass("pulse"); /* ...and add a class of .pulse to the <dt> element */
         }
         
         else { /* if the <dd> element DOES have a class of .show... */
             dd.removeClass("show"); /* ...then remove a class of .show... */
-            $(this).children("dt").removeClass("left-spin"); /* ...and also remove the class of .left-spin from the <dt> element... */
-            $(this).children("dt").addClass("right-spin"); /* ...and add a class of .right-spin */
+            $(this).children("dt").removeClass("pulse"); /* ...and remove a class of .pulse */
         }
         
     });
