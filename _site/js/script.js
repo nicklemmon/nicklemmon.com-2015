fd087d924d19c6ef75cb7f8d0c1ca1dc559c755c
Nick Lemmon <nick.lemmon@gmail.com>
2015-07-26 14:46:53 -0400

    Optimizing and smoothing section.portfolio
    - optimized some portfolio images
    - changed the script so that images are swapped our more smoothly when
    clicking on .return and then another .work-item

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,25 +1,42 @@
 $( document ).ready(function() {
-	
-	/*===== function for section.about tabs =====*/	
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
 
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
+		/*===== function for section.about tabs =====*/
+		$('ul.tabs li').click(function(){
+			var tab_id = $(this).attr('data-tab');
 
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	});
-	
-	/*===== function to load content in to .work-focus when clicking on each .work-item thumbnail ======*/ 
-	//$.ajaxSetup({ cache: true });
+			$('ul.tabs li').removeClass('current');
+			$('.tab-content').removeClass('current');
 
-	$(".work-item-unit").click(function() {
-		var worknum = $(this).attr("data-worknum"); /* declares variable that stores the data for the 'data-worknum' attribute... */
-        //var spinner = '<div class="loader">Loading...</div>';
-        
-		$(".work-focus-content").load("_includes/portfolio/work-focus-" + worknum + ".html"); /* ...which is then used to dynamically load the appropriate content */
-	});
+			$(this).addClass('current');
+			$("#"+tab_id).addClass('current');
+		});
+
+		/*===== image preloading for icons in the section.about tabs =====*/
+		$.fn.preload = function() {
+	    this.each(function(){
+	        $('<img/>')[0].src = this;
+	    });
+		}
+
+		// Usage:
+
+		$(['/images/icons/portfolio-white.svg','/images/icons/resume-white.svg']).preload();
+
+		/*===== function to load content in to .work-focus when clicking on each .work-item thumbnail ======*/
+		//$.ajaxSetup({ cache: true });
+
+		$(".work-item-unit").click(function() {
+			var worknum = $(this).attr("data-worknum"); /* declares variable that stores the data for the 'data-worknum' attribute... */
+	        //var spinner = '<div class="loader">Loading...</div>';
+
+			//$(".work-focus-content").html("<div class='loader'>Loading...</div>");
+
+			$(".work-focus-content").delay(0).queue(function() {
+				$(this).load("_includes/portfolio/work-focus-" + worknum + ".html"); /* ...which is then used to dynamically load the appropriate content */
+				$(this).dequeue();
+			})
+
+		});
 
     /*===== function for section.portfolio to bring in detailed view of each .work-item =====*/
     $(".work-item-unit").click(function() { /* when the user clicks on a .work-item... */
@@ -27,18 +44,20 @@ $( document ).ready(function() {
         $(".work-container").addClass("remove"); /* ...add the class .remove to .work-container... */
         $(".work-focus").addClass("slide"); /* ...and finally ad the class .slide to .work-focus */
     });
+
+		/*===== function to return to the page when finished viewing the detailed view for each portfolio item =====*/
     $(".return").click(function() { /* when the user clicks on .return... */
-        
-        var target = $(this).attr("id");
-        
+
+				//$(".work-focus-content").load("_includes/portfolio/work-focus-empty.html"); /* ...loads empty template for .work-focus to avoid weird image swapping issues... */
         $(".work-focus").removeClass("slide"); /* ...remove the class .slide from .work-focus... */
         $(".work-focus").addClass("remove"); /* ...add the class .remove to .work-focus... */
-        $(".work-container").delay(100).queue(function() {
-                                                $(this).removeClass("remove");
-                                                $(this).dequeue();
-                                               });  /* ...and remove the class .remove from .work-container */
+        $(".work-container").delay(400).queue(function() {
+          $(this).removeClass("remove"); /* ...removes the class .remove... */
+          $(this).dequeue();
+         });
+
     });
-    
+
     /*===== function for section.skills to 'open' each skills skill and display the <dd> =====*/
     $(".definition").click(function(){
         var dd = $(this).children("dd");
@@ -51,9 +70,9 @@ $( document ).ready(function() {
             //$(this).children("dt").removeClass("pulse"); /* ...and remove a class of .pulse */
         }
     });
-    
+
     /*===== smooth scrolling function from https://css-tricks.com/snippets/jquery/smooth-scrolling/ =====*/
-    $('a[href*=#]:not([href=#])').click(function() { 
+    $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -65,5 +84,5 @@ $( document ).ready(function() {
           }
         }
     });
-    
+
 });
--- js/script.js
+++ js/script.js
@@ -11,6 +11,15 @@ $( document ).ready(function() {
 			$("#"+tab_id).addClass('current');
 		});
 
+		/*===== image preloading for icons in the section.about tabs =====*/
+		$.fn.preload = function() {
+	    this.each(function(){
+	        $('<img/>')[0].src = this;
+	    });
+		}
+
+		$(['/images/icons/portfolio-white.svg','/images/icons/resume-white.svg']).preload();
+
 		/*===== function to load content in to .work-focus when clicking on each .work-item thumbnail ======*/
 		//$.ajaxSetup({ cache: true });
 
@@ -18,30 +27,33 @@ $( document ).ready(function() {
 			var worknum = $(this).attr("data-worknum"); /* declares variable that stores the data for the 'data-worknum' attribute... */
 	        //var spinner = '<div class="loader">Loading...</div>';
 
-			$(".work-focus-content").load("_includes/portfolio/work-focus-" + worknum + ".html"); /* ...which is then used to dynamically load the appropriate content */
+			//$(".work-focus-content").html("<div class='loader'>Loading...</div>");
+
+			$(".work-focus-content").delay(0).queue(function() {
+				$(this).load("_includes/portfolio/work-focus-" + worknum + ".html"); /* ...which is then used to dynamically load the appropriate content */
+				$(this).dequeue();
+			})
+
 		});
 
-		/*==== functions for lazy loading =====*/
-		$(function() {
-			$("img.lazy").lazyload();
-		});
-		
     /*===== function for section.portfolio to bring in detailed view of each .work-item =====*/
     $(".work-item-unit").click(function() { /* when the user clicks on a .work-item... */
         $(".work-focus").removeClass("remove"); /* ...remove the class .remove from .work-focus... */
         $(".work-container").addClass("remove"); /* ...add the class .remove to .work-container... */
         $(".work-focus").addClass("slide"); /* ...and finally ad the class .slide to .work-focus */
     });
+
+		/*===== function to return to the page when finished viewing the detailed view for each portfolio item =====*/
     $(".return").click(function() { /* when the user clicks on .return... */
 
-        var target = $(this).attr("id");
-
+				//$(".work-focus-content").load("_includes/portfolio/work-focus-empty.html"); /* ...loads empty template for .work-focus to avoid weird image swapping issues... */
         $(".work-focus").removeClass("slide"); /* ...remove the class .slide from .work-focus... */
         $(".work-focus").addClass("remove"); /* ...add the class .remove to .work-focus... */
-        $(".work-container").delay(100).queue(function() {
-                                                $(this).removeClass("remove");
-                                                $(this).dequeue();
-                                               });  /* ...and remove the class .remove from .work-container */
+        $(".work-container").delay(400).queue(function() {
+          $(this).removeClass("remove"); /* ...removes the class .remove... */
+          $(this).dequeue();
+         });
+
     });
 
     /*===== function for section.skills to 'open' each skills skill and display the <dd> =====*/
